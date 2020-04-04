f5e73e36f21052acf8e919ceea261e20a143097c
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-04-04 16:54:19 +0300

    Add sample feature to populate listbox with messages

---
+++ src/Chat/Chat.GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Chat.Core;
+using Chat.GUI.MVVM;
+using Chat.GUI.MVVM.Commands;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Chat.GUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<MessageViewModel> Messages { get; set; }
+
+        private ICommand _addMessageCommand;
+        public ICommand AddMessageCommand
+        {
+            get
+            {
+                if (_addMessageCommand == null)
+                    _addMessageCommand = new RelayCommand(_ => { AddSampleMessage(); });
+
+                return _addMessageCommand;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Messages = new ObservableCollection<MessageViewModel>();
+        }
+
+        private void AddSampleMessage()
+        {
+            var sampleMessage = new Message("Anonymous", "Big Brother is watching you.", DateTime.Now);
+            Messages.Add(new MessageViewModel(sampleMessage));
+        }
+    }
+}
---
+++ src/Chat/Chat.GUI/ViewModels/MessageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Chat.Core;
+using Chat.GUI.MVVM;
+
+namespace Chat.GUI.ViewModels
+{
+    public class MessageViewModel : ViewModelBase
+    {
+        private readonly Message _message;
+
+        private string _author;
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                _author = value;
+                OnPropertyChanged("Author");
+            }
+        }
+
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        private string _timestamp;
+        public string Timestamp
+        {
+            get { return _timestamp; }
+            set
+            {
+                _timestamp = value;
+                OnPropertyChanged("Timestamp");
+            }
+        }
+
+        public MessageViewModel(Message message)
+        {
+            this._message = message;
+
+            Author = message.Author;
+            Text = message.Text;
+            Timestamp = message.Timestamp.ToShortTimeString();
+        }
+    }
+}
