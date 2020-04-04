181b41fce8470245aaec1e2d460f787eff9194b7
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-04-04 16:10:43 +0300

    Add Chat.Core project and Message class

---
+++ src/Chat/Chat.Core/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chat.Core
+{
+    [Serializable]
+    public class Message
+    {
+        public string Author { get; }
+        public string Text { get; }
+        public DateTime Timestamp { get; }
+
+        public Message(string author, string text, DateTime timestamp)
+        {
+            Author = author;
+            Text = text;
+            Timestamp = timestamp;
+        }
+    }
+}
