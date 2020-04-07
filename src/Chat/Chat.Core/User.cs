a7e4e057406999bf7849d660eb8a80a99019bb00
Виктор Михайлов <victormihailov@hotmail.com>
2020-04-07 22:32:10 +0300

    Create class User with property and enum

---
+++ src/Chat/Chat.Core/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Core
+{
+    public class User
+    {
+        private UserState State { get; set; } //почему здесь модификатор private?
+
+        enum UserState
+        {
+            Available,
+            Offline,
+            Away
+        };
+    }
+}
+}
